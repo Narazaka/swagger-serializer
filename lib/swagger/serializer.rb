9a4dc06e209e95cc9515064914e73d775d0553d6
Narazaka <info@narazaka.net>
2019-11-26 16:23:13 +0900

    init

---
+++ lib/swagger/serializer.rb
@@ -0,0 +1,8 @@
+require "swagger/serializer/version"
+
+module Swagger
+  module Serializer
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/swagger/serializer/version.rb
@@ -0,0 +1,5 @@
+module Swagger
+  module Serializer
+    VERSION = "0.1.0"
+  end
+end
