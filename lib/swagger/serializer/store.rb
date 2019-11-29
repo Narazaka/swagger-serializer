9e239a7904fe987f8369809cd40d82aebfb0b019
Narazaka <info@narazaka.net>
2019-11-30 07:47:52 +0900

    cache

--- lib/swagger/serializer/store.rb
+++ lib/swagger/serializer/store.rb
@@ -7,20 +7,34 @@ module Swagger
         end
       end
 
-      attr_accessor :inject_key, :injectors
+      attr_accessor :inject_key, :injectors, :cache
 
-      def initialize(inject_key = "title", injectors = Injectors.new)
+      def initialize(inject_key: "title", injectors: nil, cache: false)
         @inject_key = inject_key
-        @injectors = injectors
+        @injectors = injectors || Injectors.new(cache: cache)
+        @cache = cache
       end
 
       def serializer_options
-        { inject_key: inject_key, injectors: injectors }
+        if cache
+          @serializer_options ||= { inject_key: inject_key, injectors: injectors }
+        else
+          { inject_key: inject_key, injectors: injectors }
+        end
       end
 
       class Injectors
+        def initialize(injectors: {}, cache: false)
+          @injectors = injectors
+          @cache = cache
+        end
+
         def [](name)
-          "#{name}Serializer".safe_constantize
+          if @cache
+            @injectors[name] ||= "#{name}Serializer".safe_constantize
+          else
+            "#{name}Serializer".safe_constantize
+          end
         end
       end
     end
